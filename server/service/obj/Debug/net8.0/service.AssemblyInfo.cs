6443a8ac0ee053f021ddee192bf5168372d0cc35
Mads Fjand Møller <mads@Madss-iMac.local>
2024-10-11 21:53:04 +0200

    small changes

--- server/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ server/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d0e5bbf6d4b2c61ce8e81ce5c3de88acde7fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e479b3a7a0901193c6f2485ec599cec73dc6f03")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

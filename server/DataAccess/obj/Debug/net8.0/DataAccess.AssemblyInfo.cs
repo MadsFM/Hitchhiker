cab37dd4a649bd756e8d80bf1f389b8b67cbbe09
MadsFM <madsmoeller13@gmail.com>
2024-10-18 06:56:10 +0200

    changes from remote desktop

--- server/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ server/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d0e5bbf6d4b2c61ce8e81ce5c3de88acde7fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8fb01560ca2fd9cf8499360ab2c3d57da104689")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

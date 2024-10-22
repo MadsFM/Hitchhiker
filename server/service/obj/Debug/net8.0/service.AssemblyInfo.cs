434e9c7a67ebc517144b5d584f869b4f222a5624
MadsFM <madsmoeller13@gmail.com>
2024-10-22 22:53:20 +0200

    merge conflict

--- server/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ server/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e150d49d3b5ceabc28b8e34f26f4f5d20b00dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b14031cb4112886fff18fc03c33c16e177abb1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

5f3ec4498c81ff2d05360c091a7ef71ace0e1ade
MadsFM <madsmoeller13@gmail.com>
2024-10-24 22:00:26 +0200

    made a class for testing the API

--- server/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ server/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b14031cb4112886fff18fc03c33c16e177abb1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77e4d020f73a7b27d2ef41c637a6dd6ca861244")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
